841217423827f9b75684521210631b89c362c297
Valentin Baca <valbaca@gmail.com>
2022-07-03 10:14:46 -0700

    use fetch() instead of Spring custom api fetcher

--- src/main/js/app.jsx
+++ src/main/js/app.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import client from './api/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PetList from "./components/petList.jsx";
 import './app.css';
@@ -12,13 +11,13 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        client({
-            method: 'GET', path: '/api/pets',
-        }).then(response => {
-            this.setState({
-                pets: response.entity._embedded.pets
+        fetch('api/pets')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    pets: data._embedded.pets
+                });
             });
-        });
     }
 
     render() {
